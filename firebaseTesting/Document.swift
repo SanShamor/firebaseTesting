b1048a5bb082b95e3b8018149bafc95551452a78
SanShamor <sanshamor@mail.ru>
2021-10-14 15:31:24 +0300

    imoprted firebase, default picture

---
+++ firebaseTesting/Document.swift
@@ -0,0 +1,13 @@
+//
+//  Document.swift
+//  firebaseTesting
+//
+//  Created by Александр on 14.10.2021.
+//
+
+import Foundation
+
+struct Document {
+    let field1: String
+    let field2: String
+}
--- firebaseTesting/ViewController.swift
+++ firebaseTesting/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var surnameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showFulgrimButtonPressed(_ sender: Any) {
+        APIManager.shared.getPost(collection: "warriors", docName:  "emperorsChildren", completion: {doc in
+            guard doc != nil else { return }
+            self.nameLabel.text = doc?.field1
+            self.surnameLabel.text = doc?.field2
+        })
+        APIManager.shared.getImage(picName: "Fulgrim", completion: { pic in
+            self.picture.image = pic
+        })
+    }
+    
+    @IBAction func showRogalDornButtonPressed(_ sender: Any) {
+        APIManager.shared.getPost(collection: "warriors", docName:  "imperialFists", completion: {doc in
+            guard doc != nil else { return }
+            self.nameLabel.text = doc?.field1
+            self.surnameLabel.text = doc?.field2
+        })
+        APIManager.shared.getImage(picName: "Rogal", completion: { pic in
+            self.picture.image = pic
+        })
+    }
+    
+    @IBAction func showLemanRussButtonPressed(_ sender: Any) {
+        APIManager.shared.getPost(collection: "warriors", docName:  "spaceWolves", completion: {doc in
+            guard doc != nil else { return }
+            self.nameLabel.text = doc?.field1
+            self.surnameLabel.text = doc?.field2
+        })
+        APIManager.shared.getImage(picName: "Leman", completion: { pic in
+            self.picture.image = pic
+        })
+    }
+    
 
 }
 
